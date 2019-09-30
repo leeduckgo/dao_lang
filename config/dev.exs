63c1a2c0fcd025f66b4f7fc7f8c27d5be3a41c78
leeduckgo <albertschr@163.com>
2019-09-30 21:04:09 +0800

    feat: add hell world

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,8 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :dao, DaoWeb.Endpoint,
-  http: [port: 4000],
+  url: [host: "149.28.86.38", port: 4000],
+  # http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
